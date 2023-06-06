85b663cbe54dfd0396995e8721484b6ef3ece2c6
tennil <kangethelinnet@gmail.com>
2023-06-06 16:59:19 -0700

    prints different combinations of two digits.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for j in range(0, 10):
         if j == 8 and k == 9:
             print("{:d}{:d}".format(j, k))
         elif j < k and j != k:
-            print("{:d}{:d},".format(j, k), end='')
+            print("{:d}{:d}, ".format(j, k), end='')
